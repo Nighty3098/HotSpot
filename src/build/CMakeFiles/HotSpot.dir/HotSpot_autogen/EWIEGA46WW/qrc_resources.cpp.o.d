7a59d5237b9a222da1ac3eb2cd4ef3ecc09fdc64
Artem <154594695+Nighty3098@users.noreply.github.com>
2025-11-10 13:33:16 +0700

    Fix

--- src/build/CMakeFiles/HotSpot.dir/HotSpot_autogen/EWIEGA46WW/qrc_resources.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/HotSpot.dir/HotSpot_autogen/EWIEGA46WW/qrc_resources.cpp.o: \
- /home/nighty/Dev/HotSpot/src/build/HotSpot_autogen/EWIEGA46WW/qrc_resources.cpp \
- /usr/include/stdc-predef.h
